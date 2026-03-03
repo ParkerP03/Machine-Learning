83ab15819f33810c55baa60d248e1a9d022d6e69
ParkerP03 <php.nola@gmail.com>
2026-03-03 17:59:32 -0600

    Reorganize imports and update feature extraction functions

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -53,7 +53,7 @@ def extract_features_pair():
 
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['AAPL', 'MPWR']
+    stk_tickers = ['AAPL', 'AME']
     
     stk_data = yf.download(stk_tickers, start=START_DATE, end=END_DATE, auto_adjust=False)
 
@@ -87,4 +87,5 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
