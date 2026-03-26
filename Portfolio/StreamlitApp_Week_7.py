0b459d85110819ba008c509de19011f30a5cd31e
ParkerP03 <php.nola@gmail.com>
2026-03-26 18:01:13 -0500

    Refactor AWS model configuration and update input keys

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["AME", "AAPL"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AME", "AAPL"]]
+        "keys": ["AOS", "FCX"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["AOS", "FCX"]]
 }
 
 def load_pipeline(_session, bucket, key):
